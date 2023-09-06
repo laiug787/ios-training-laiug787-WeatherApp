8423d32fe3dfbb6ab8d12b310b320bed2edb765c
SegawaYusho <laiug787@icloud.com>
2023-09-06 18:03:29 +0900

    delete: viewDidLoad & viewDidLayoutSubviews

--- WhetherApp/LaunchViewController.swift
+++ WhetherApp/LaunchViewController.swift
@@ -8,10 +8,6 @@
 import UIKit
 
 final class LaunchViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -22,12 +18,4 @@ final class LaunchViewController: UIViewController {
         self.present(nextVC, animated: true, completion: nil)
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-
-        let storyboard = UIStoryboard(name: "WeatherView", bundle: nil)
-        let nextVC = storyboard.instantiateViewController(identifier: "WeatherView")
-        nextVC.modalPresentationStyle = .fullScreen
-        self.present(nextVC, animated: true, completion: nil)
-    }
 }
