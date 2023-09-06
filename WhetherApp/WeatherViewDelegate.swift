13553e8c0eabdff94b00719e1b3b2719a110ed09
SegawaYusho <laiug787@icloud.com>
2023-09-06 19:32:11 +0900

    add: WeatherViewDelegate

---
+++ WhetherApp/WeatherViewDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherViewDelegate.swift
+//  WhetherApp
+//
+//  Created by 瀬川 裕翔 on 2023/09/06.
+//
+
+import Foundation
+
+protocol WeatherViewDelegate: AnyObject {
+    func closeWeather()
+    func reloadWeather()
+}
+
+
