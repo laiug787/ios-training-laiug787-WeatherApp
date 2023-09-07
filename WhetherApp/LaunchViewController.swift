a5e5b2ebf761f16fa10f89d19fd6d42b754dab34
Yusho <86225588+laiug787@users.noreply.github.com>
2023-09-07 10:18:29 +0900

    Update WhetherApp/LaunchViewController.swift
    Co-authored-by: KAWASHIMA Yoshiyuki <ykawashi7@gmail.com>

--- WhetherApp/LaunchViewController.swift
+++ WhetherApp/LaunchViewController.swift
@@ -15,7 +15,7 @@ final class LaunchViewController: UIViewController {
         let storyboard = UIStoryboard(name: "WeatherView", bundle: nil)
         let nextVC = storyboard.instantiateViewController(identifier: "WeatherView")
         nextVC.modalPresentationStyle = .fullScreen
-        self.present(nextVC, animated: true, completion: nil)
+        self.present(nextVC, animated: true)
     }
     
 }
