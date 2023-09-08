2ccd2fed667e1ce8f397d9223cc4290cfff214f2
SegawaYusho <laiug787@icloud.com>
2023-09-08 19:56:43 +0900

    update: use WeatherRepositoryProtocol

--- WhetherApp/WeatherViewController.swift
+++ WhetherApp/WeatherViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 // MARK: ViewController
 final class WeatherViewController: UIViewController {
     
-    private var repository = WeatherRepository()
+    private var repository: WeatherRepositoryProtocol!
     
     @IBOutlet @ViewLoading private var weatherImage: UIImageView
 
@@ -24,6 +24,7 @@ final class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        repository = WeatherRepository()
         repository.delegate = self
     }
     
