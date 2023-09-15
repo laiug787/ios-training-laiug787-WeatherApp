448640124b92df7492ff9de1607daf9fc01c258e
SegawaYusho <laiug787@icloud.com>
2023-09-15 18:29:44 +0900

    update: unitTest

--- WeatherAppTests/WeatherAppTests.swift
+++ WeatherAppTests/WeatherAppTests.swift
@@ -11,12 +11,16 @@ import XCTest
 final class MockWeatherRepositry: WeatherRepositoryProtocol {
     weak var delegate: WhetherApp.WeatherRepositoryDelegate?
     
-    let weatherData = WeatherData(
-        date: "",
-        maxTemperature: 1,
-        minTemperature: 1,
-        weatherCondition: .sunny
-    )
+    let weatherData: WeatherData
+    
+    init(maxTemperature: Int = 10, minTemperature: Int = 20, weatherCondition: WeatherCondition = .sunny) {
+        self.weatherData = WeatherData(
+            date: "",
+            maxTemperature: maxTemperature,
+            minTemperature: minTemperature,
+            weatherCondition: weatherCondition
+        )
+    }
     
     func fetchWeatherCondition(area: String, date: Date) {
         delegate?.weatherRepository(self, didFetchWeatherData: weatherData)
@@ -26,21 +30,64 @@ final class MockWeatherRepositry: WeatherRepositoryProtocol {
 final class WeatherAppTests: XCTestCase {
     
     func testWeatherImageIsSunny() {
-        let repository = MockWeatherRepositry()
+        let repository = MockWeatherRepositry(weatherCondition: .sunny)
         let storyBoard = UIStoryboard(name: "WeatherView", bundle: nil)
         let viewController = storyBoard.instantiateViewController(identifier: "WeatherView") { coder in
             return WeatherViewController(coder: coder, repository: repository)
         }
-
-        guard let imageView = viewController.weatherImage else { return }
-        
         viewController.loadViewIfNeeded()
-        
         viewController.reloadAction(self)
         
-        XCTAssertNotNil(imageView.image)
-        
-        XCTAssertEqual(imageView.image, UIImage(named: "weather-sunny"))
+        XCTAssertEqual(viewController.weatherImage.image, UIImage.sunny)
+    }
+    
+    func testWeatherImageIsCloudy() {
+        let repository = MockWeatherRepositry(weatherCondition: .cloudy)
+        let storyBoard = UIStoryboard(name: "WeatherView", bundle: nil)
+        let viewController = storyBoard.instantiateViewController(identifier: "WeatherView") { coder in
+            return WeatherViewController(coder: coder, repository: repository)
+        }
+        viewController.loadViewIfNeeded()
+        viewController.reloadAction(self)
+
+        XCTAssertEqual(viewController.weatherImage.image, UIImage.cloudy)
     }
 
+    func testWeatherImageIsRainy() {
+        let repository = MockWeatherRepositry(weatherCondition: .rainy)
+        let storyBoard = UIStoryboard(name: "WeatherView", bundle: nil)
+        let viewController = storyBoard.instantiateViewController(identifier: "WeatherView") { coder in
+            return WeatherViewController(coder: coder, repository: repository)
+        }
+        viewController.loadViewIfNeeded()
+        viewController.reloadAction(self)
+
+        XCTAssertEqual(viewController.weatherImage.image, UIImage.rainy)
+    }
+
+    func testMaxTemperatureLabelHasText() {
+        let maxTemperature = 30
+        let repository = MockWeatherRepositry(maxTemperature: maxTemperature)
+        let storyBoard = UIStoryboard(name: "WeatherView", bundle: nil)
+        let viewController = storyBoard.instantiateViewController(identifier: "WeatherView") { coder in
+            return WeatherViewController(coder: coder, repository: repository)
+        }
+        viewController.loadViewIfNeeded()
+        viewController.reloadAction(self)
+
+        XCTAssertEqual(viewController.maxTemperatureLabel.text, "\(maxTemperature)")
+    }
+
+    func testMinTemperatureLabelHasText() {
+        let minTemperature = 0
+        let repository = MockWeatherRepositry(minTemperature: minTemperature)
+        let storyBoard = UIStoryboard(name: "WeatherView", bundle: nil)
+        let viewController = storyBoard.instantiateViewController(identifier: "WeatherView") { coder in
+            return WeatherViewController(coder: coder, repository: repository)
+        }
+        viewController.loadViewIfNeeded()
+        viewController.reloadAction(self)
+
+        XCTAssertEqual(viewController.minTemperatureLabel.text, "\(minTemperature)")
+    }
 }
